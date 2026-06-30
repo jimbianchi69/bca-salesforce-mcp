8f2ab2249245f031f61c6db8d3d639b2cf633a33
jimbianchi69 <jim@bcafinancialgroup.com>
2026-06-30 17:39:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -423,6 +423,15 @@ function buildServer() {
 }
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(204);
+  next();
+});
+
 app.use("/mcp", express.raw({ type: "application/json" }));
 app.use(express.json());
 
